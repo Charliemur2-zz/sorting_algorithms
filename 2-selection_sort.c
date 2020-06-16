327d1f0b74a248fb91d553ee8e85051b3352d5e5
Charliemur2 <charliemurciamunoz@gmail.com>
2020-06-16 20:43:26 +0000

    create 2-selection_sort.c 2-O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,26 +6,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    unsigned int i = 0, j, k;
-    int tmp;
+	unsigned int i = 0, j, k;
+	int tmp;
 
 	if (array != NULL && size > 1)
-        while (i <= (size - 2))
-        {
-            tmp = array[i];
-            k = i;
-            for (j = i + 1; j < size; j++)
-            {
-                if (tmp > array[j])
-                {
-                    tmp = array[j];
-                    k = j;
-                }
-            }
-            array[k] = array[i];
-            array[i] = tmp;
-            if (k != i)
-                print_array(array, size);
-            i++;
-        }		
+	{
+		while (i <= (size - 2))
+		{
+			tmp = array[i];
+			k = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (tmp > array[j])
+				{
+					tmp = array[j];
+					k = j;
+				}
+			}
+			array[k] = array[i];
+			array[i] = tmp;
+			if (k != i)
+				print_array(array, size);
+			i++;
+		}
+	}
 }
