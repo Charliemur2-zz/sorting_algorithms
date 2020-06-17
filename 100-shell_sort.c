c12e2afcc06f01794a1a16fdb43c202c31ed879e
Cristiand187 <cristiand187@hotmail.com>
2020-06-17 21:44:04 +0000

    Task 4 - Add sort shell

---
+++ 100-shell_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * shell_sort - function that sorts an array of integers in
+ * ascending order using the Shell sort
+ * @array: array the number
+ * @size: len the array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	unsigned int gap = 1, i = 0, j = 0;
+	int temp = 0;
+
+	gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j = j - gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		gap /= 3;
+		print_array(array, size);
+	}
+}
