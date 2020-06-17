e4d03392aef15c043184518b4254d993ab416a96
Cristiand187 <cristiand187@hotmail.com>
2020-06-17 22:23:47 +0000

    Task 4 - Add sort shell

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -13,13 +13,15 @@ void shell_sort(int *array, size_t size)
 
 	if (array != NULL && size > 1)
 	{
-		gap = gap * 3 + 1;
+		while (gap < size / 3)
+      			gap = gap * 3 + 1;
+
 		while (gap > 0)
 		{
 			for (i = gap; i < size; i++)
 			{
 				temp = array[i];
-				for (j = i; j > gap - 1 && array[j - gap] > temp; j = j - gap)
+				for (j = i; j >= gap && array[j - gap] > temp; j = j - gap)
 				{
 					array[j] = array[j - gap];
 				}
